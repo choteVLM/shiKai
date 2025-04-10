cda9c0277ed773d92d679492998a803cdf4d47b8
rethinkNow <aryanjain85297@gmail.com>
2025-04-10 13:29:44 +0530

    refactored asr code

---
+++ modelASR/base_model.py
@@ -0,0 +1,9 @@
+class base_model:
+    def __init__(self):
+        return
+
+    def transcribe_audio(self, audio_path:str, sample_rate:int):
+        return
+    
+    def transcribe_batch_audio(self, chunk_files, sample_rate:int):
+        return
